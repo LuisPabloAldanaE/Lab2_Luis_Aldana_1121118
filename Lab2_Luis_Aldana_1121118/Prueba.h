5705e32c0e75e5d03d6e569f94297c45dcb09f28
LuisPabloAldanaE <70825058+LuisPabloAldanaE@users.noreply.github.com>
2020-09-12 07:54:44 -0600

    Label

--- Lab2_Luis_Aldana_1121118/Prueba.h
+++ Lab2_Luis_Aldana_1121118/Prueba.h
@@ -34,6 +34,8 @@ namespace Lab2LuisAldana1121118 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,11 +50,29 @@ namespace Lab2LuisAldana1121118 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Prueba";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(132, 111);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(46, 17);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"label1";
+			// 
+			// Prueba
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(282, 253);
+			this->Controls->Add(this->label1);
+			this->Name = L"Prueba";
+			this->Text = L"Prueba";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
